92090c26071475f4190742ee3cc8f77c245cfc8d
Nils Erlend Heggem <nilsheggem@hotmail.com>
2019-05-06 20:16:20 +0200

    Fixed syntax bug

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var list = ["Hello!", "Goodbye!", "Soup", "Mars"];
-var i = Math.floor(Math.random() * 4;
+var i = Math.floor(Math.random() * 4);
 str = list[i];
 document.getElementById("str").innerHTML = str;
